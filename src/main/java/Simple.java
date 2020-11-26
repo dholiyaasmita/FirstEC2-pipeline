8ed10bfb36dba7f65b7043453e4d069dd7747b93
Asmi <maredia1990@gmail.com>
2020-11-26 12:54:09 -0500

    Finally done with Jenkins

--- src/main/java/Simple.java
+++ src/main/java/Simple.java
@@ -1,5 +1,6 @@
 public class Simple {
     public static void main(String[] args) {
         System.out.println("Don't know anything about jenkins");
+        System.out.println("Took 3 days to solve.......");
     }
 }
